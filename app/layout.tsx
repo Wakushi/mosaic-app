9e17d204d7f69834057a8b1fcf9122ec113d20a0
Camillemtd <camille_mtd@hotmail.com>
2024-04-30 12:57:36 -0400

    Add RainbowKit integration for wallet support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "./provider";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <header>
+            <Header/>
+          </header>
+          <main>{children}</main>
+        </Providers>
+      </body>
     </html>
   );
 }
