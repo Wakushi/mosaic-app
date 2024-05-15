e3e159ba385db67edaa7c5cf30129230bf412cdc
Camillemtd <camille_mtd@hotmail.com>
2024-05-15 14:08:35 -0400

    link home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,11 @@
-"use client"
+"use client";
 import { useState } from "react";
+import Link from "next/link";
+
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 import { Button } from "./ui/button";
+
 import { Modal } from "@/components/clientUi/modal";
 import { ProfileForm } from "./profile-form";
 export default function Header() {
@@ -9,25 +13,26 @@ export default function Header() {
   const toggleModal = () => setModalOpen(!modalOpen);
   return (
     <div className="flex justify-between p-3 z-30 w-screen">
-      <div>Mosaic</div>
+      <Link href="/">
+        <div>Mosaic</div>
+      </Link>
       <div className="flex gap-4">
         <Button onClick={toggleModal}>Complete your register</Button>
         <Modal isOpen={modalOpen} close={toggleModal}>
-        <ProfileForm/> 
-      </Modal>
+          <ProfileForm />
+        </Modal>
         <ConnectButton
-        accountStatus={{
-          smallScreen: "avatar",
-          largeScreen: "avatar",
-        }}
-        showBalance={{
-          smallScreen: false,
-          largeScreen: false,
-        }}
-        chainStatus="none"
-      />
+          accountStatus={{
+            smallScreen: "avatar",
+            largeScreen: "avatar",
+          }}
+          showBalance={{
+            smallScreen: false,
+            largeScreen: false,
+          }}
+          chainStatus="none"
+        />
       </div>
-      
     </div>
   );
 }
