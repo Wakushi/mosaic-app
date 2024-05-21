76f9ffb854194e00536d8591589823ab90fc1bd0
makushi <mzozord@gmail.com>
2024-05-21 19:32:52 +0200

    Updated certificat

--- components/certificat-authentificity.tsx
+++ components/certificat-authentificity.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React from "react"
 
 interface CertificateAuthentificityProps {
-  title: string;
-  artist: string;
+  title: string
+  artist: string
 }
 
-const CertificateAuthentificity: React.FC<CertificateAuthentificityProps> = ({ title, artist }) => {
+const CertificateAuthentificity: React.FC<CertificateAuthentificityProps> = ({
+  title,
+  artist,
+}) => {
   return (
-    <div style={{ width: '1200px', height: '600px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', border: '2px solid black', padding: '20px', fontFamily: 'Roboto' }}>
-      <h1 style={{ fontSize: '48px', marginBottom: '20px' }}>Certificate of Authenticity</h1>
-      <p style={{ fontSize: '60px' }}>Title: {title}</p>
-      <p style={{ fontSize: '60px' }}>Artist: {artist}</p>
+    <div
+      style={{
+        width: "800px",
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "2px solid #000",
+        padding: "20px",
+        fontFamily: "Roboto",
+        color: "#fff",
+        backgroundColor: "#000",
+      }}
+    >
+      <h1 style={{ fontSize: "28px", marginBottom: "20px" }}>
+        Certificate of Authenticity
+      </h1>
+      <p style={{ fontSize: "20px" }}>Title: {title}</p>
+      <p style={{ fontSize: "20px" }}>Artist: {artist}</p>
     </div>
-  );
-};
+  )
+}
 
-export default CertificateAuthentificity;
+export default CertificateAuthentificity
