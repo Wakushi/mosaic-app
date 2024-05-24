4d8134c680ea2f8bdbb64a4ad9f2c6011791c360
makushi <mzozord@gmail.com>
2024-05-24 13:17:27 +0200

    Added mosaic logo to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,75 +1,93 @@
-"use client";
+"use client"
 
 // React
-import { useEffect, useState } from "react";
-import Link from "next/link";
+import { useEffect, useState } from "react"
+import Link from "next/link"
 
 // Wagmi
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount } from "wagmi";
+import { ConnectButton } from "@rainbow-me/rainbowkit"
+import { useAccount } from "wagmi"
 
 // Components
-import { Modal } from "@/components/clientUi/modal";
-import { ProfileForm } from "./profile-form";
+import { Modal } from "@/components/clientUi/modal"
+import { ProfileForm } from "./profile-form"
 
 // Shadcn
-import { Button } from "./ui/button";
+import { Button } from "./ui/button"
 import {
   DropdownMenu,
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuShortcut,
-} from "./ui/dropdown-menu";
+} from "./ui/dropdown-menu"
 
 // Zustand store
-import { useUserStore } from "@/store/useStore"; 
+import { useUserStore } from "@/store/useStore"
+import Image from "next/image"
 
 const fetchUserData = async (clientAddress: string) => {
-  const response = await fetch(`/api/user?clientAddress=${clientAddress}`);
-  const data = await response.json();
+  const response = await fetch(`/api/user?clientAddress=${clientAddress}`)
+  const data = await response.json()
   if (!response.ok) {
-    throw new Error(data.error || 'Failed to fetch user data');
+    throw new Error(data.error || "Failed to fetch user data")
   }
-  return data;
-};
+  return data
+}
 
 export default function Header() {
-  const account = useAccount();
-  const [modalOpen, setModalOpen] = useState(false);
+  const account = useAccount()
+  const [modalOpen, setModalOpen] = useState(false)
 
-  const { isRegistered, isAdmin, userType, setIsRegistered, setIsAdmin, setUserType } = useUserStore();
+  const {
+    isRegistered,
+    isAdmin,
+    userType,
+    setIsRegistered,
+    setIsAdmin,
+    setUserType,
+  } = useUserStore()
 
-  const clientAddress = account.address;
+  const clientAddress = account.address
 
   useEffect(() => {
     async function checkUser() {
       if (clientAddress) {
         try {
-          const data = await fetchUserData(clientAddress);
-          setIsRegistered(data.isRegistered);
+          const data = await fetchUserData(clientAddress)
+          setIsRegistered(data.isRegistered)
 
           if (data.isRegistered) {
-            const user = data.user;
-            setIsAdmin(user.role === "admin");
-            setUserType(user.userType);
+            const user = data.user
+            setIsAdmin(user.role === "admin")
+            setUserType(user.userType)
           }
         } catch (error) {
-          console.error("Error fetching user data:", error);
-          setIsRegistered(false);
+          console.error("Error fetching user data:", error)
+          setIsRegistered(false)
         }
       }
     }
 
-    checkUser();
-  }, [clientAddress]);
+    checkUser()
+  }, [clientAddress])
 
-  const toggleModal = () => setModalOpen(!modalOpen);
+  const toggleModal = () => setModalOpen(!modalOpen)
 
   return (
     <div className="flex justify-between py-3 z-30 w-screen fixed px-14 items-center">
       <Link href="/">
-        <div>Mosaic</div>
+        <div className="w-[200px]">
+          <Image
+            src="/images/logo/mosaic-logo.png"
+            alt="Mosaic logo"
+            width={0}
+            height={0}
+            style={{ width: "100%", height: "100%" }}
+            sizes="100vw"
+            className="object-cover"
+          />
+        </div>
       </Link>
       <div className="flex gap-4">
         <Link href="/marketplace">
@@ -126,5 +144,5 @@ export default function Header() {
         />
       </div>
     </div>
-  );
+  )
 }
