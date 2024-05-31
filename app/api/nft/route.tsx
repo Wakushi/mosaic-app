c365b129fe308e5e7d06a1d7199ea9937648023a
makushi <mzozord@gmail.com>
2024-05-31 16:30:56 +0200

    Fixed moralis nft fetch

--- app/api/nft/route.tsx
+++ app/api/nft/route.tsx
@@ -1,4 +1,5 @@
 import { DWORK_SHARES_ADDRESS_POLYGON } from "@/lib/contract"
+import { MoralisNft } from "@/types/moralis-nft"
 import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
@@ -17,17 +18,21 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
     if (!apiKey) {
       throw new Error("MORALIS_API_KEY is not defined")
     }
-
-    const nftRequest = await fetch(
-      `https://deep-index.moralis.io/api/v2.2/${clientAddress}/nft?chain=polygon%20amoy&token_addresses=${DWORK_SHARES_ADDRESS_POLYGON}`,
-      {
-        headers: {
-          "x-api-key": apiKey,
-        },
-      }
-    )
+    const url = `https://deep-index.moralis.io/api/v2.2/${clientAddress}/nft?chain=polygon%20amoy`
+    console.log("API request:", url)
+    const nftRequest = await fetch(url, {
+      headers: {
+        "x-api-key": apiKey,
+      },
+    })
     const response = await nftRequest.json()
-    return NextResponse.json(response.result)
+    const filteredNfts = response.result.filter((nft: MoralisNft) => {
+      return (
+        nft.token_address.toLowerCase() ===
+        DWORK_SHARES_ADDRESS_POLYGON.toLowerCase()
+      )
+    })
+    return NextResponse.json(filteredNfts)
   } catch (error) {
     console.error("API error:", error)
     if (error instanceof Error) {
